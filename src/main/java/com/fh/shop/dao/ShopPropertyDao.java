268e8165c84998f21117fab52f5a9e843f3abb93
liudi <liudi6800@163.com>
2021-01-14 21:16:50 +0800

    跟新删除接口  改为假删除

--- src/main/java/com/fh/shop/dao/ShopPropertyDao.java
+++ src/main/java/com/fh/shop/dao/ShopPropertyDao.java
@@ -3,6 +3,7 @@ package com.fh.shop.dao;
 import com.fh.shop.entity.po.ShopProperty;
 import com.fh.shop.entity.vo.ShopPropertyParms;
 import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -17,6 +18,6 @@ public interface ShopPropertyDao {
 
     void updateShopProperty(ShopProperty shopProperty);
 
-    @Delete("delete from s_shop_property where id=#{id}")
+    @Update("update s_shop_property set isDel=1  where id=#{id}")
     void deleteShopPropertyById(Integer id);
 }

5028f20893045e5efb2c02168eccdf4705c6f9d3
liudi <liudi6800@163.com>
2021-01-16 16:14:06 +0800

    处理一下阿里安全问题

--- src/main/java/com/fh/shop/utils/FileText.java
+++ src/main/java/com/fh/shop/utils/FileText.java
@@ -19,8 +19,8 @@ public class FileText {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "http://oss-cn-beijing.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        String accessKeyId = "LTAI4GJs4jmAirJGGQQcK7FK";
-        String accessKeySecret = "2CXp2A5GIpddPrywh9VRqK72Mnvuwu";
+        String accessKeyId = "LTAI4G6aPTa7yNCJrgFK7hkA";
+        String accessKeySecret = "YNvvQbfu0ZDD5JC96OOrOB23REHNQB";
         String bucketName = "imagestext";
 
         // 创建OSSClient实例。
